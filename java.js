ad4e7da9853303e83152fdb49ccd068b6616b9bb
programmer1234art <shivamsingh280912@gmail.com>
2025-05-15 20:43:02 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+const product = {
+    title : "ball pen",
+    rate : 4,
+offer: 5,
+price: 270,
+
+    
+};
+console.log(product)
+
+
+
+
+const shivam = {
+fullname: "shivam singh",
+isfollow:true,
+follower:5,
+post:195,
+motivationline:"make a discipline in your life",
+};
+
+console.log(shivam)
